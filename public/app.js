46e0570c6abc5486d2269f5c0b083f68cecdeb9d
Justin Clark <justclark.jose@gmail.com>
2019-04-15 18:27:58 -0500

    cards and button works

--- public/app.js
+++ public/app.js
@@ -1,27 +1,29 @@
 // Grab the articles as a json
 $.getJSON("/articles", function(data) {
+  console.log(data)
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").append('<div class="card-body" data-id="' + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</div>");
+      $("#articles").append(
+        
+        "<div class=\"card\" data-id=\"" + data[i]._id + "\">" 
+        + data[i].title + "<br />" 
+        + data[i].link + "</div>"
+        );
 
-      
-      $('.card-body').find(".item[data-id='" + data[i]._id + "']").append("<button>Test</button>");
+
+      $('.card[data-id="'+ data[i]._id + '"]').append("<button>Test</button>");
     }
   });
   
   
-  // Whenever someone clicks a p tag
+  // Whenever someone clicks the scrape tag
   $(document).on("click", "#scrapeBtn", function() {
-    // Empty the notes from the note section
-    $("#notes").empty();
-    // Save the id from the p tag
-    var thisId = $(this).attr("data-id");
-  
+   
     // Now make an ajax call for the Article
     $.ajax({
       method: "GET",
-      url: "/articles/" + thisId
+      url: "/scrape"
     })
       // With that done, add the note information to the page
       .then(function(data) {
